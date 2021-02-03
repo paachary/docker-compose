d7142faa5749c70fc152c64bbb7958db5b10b245
paachary <prashant_acharya74@yahoo.com>
2021-02-03 21:51:25 +0530

    changes

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,9 +1,1 @@
-sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
-
-docker-compose --version
-
 docker-compose up
--- pre-install.sh
+++ pre-install.sh
@@ -2,5 +2,9 @@
 sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
-sudo usermod -a -G docker cloud_user
+sudo usermod -a -G docker ec2-user
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -sf /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
